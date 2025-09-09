00a0ffc317102564f3a7636ecbb1c3e95278c62f
IANYEYZ <Ian.ye01@outlook.com>
2025-09-09 23:12:57 +0800

    Add Input Class & Fix Bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import { $, World } from "./waffle.js";
 
-const app = new World($('#app'))
+const app = new World($('#app'), 60)
 
 const obj1 = app.spawn({
     tags: ["pos"],
@@ -9,3 +9,11 @@ const obj1 = app.spawn({
 })
 
 console.log(app.q().with(["pos"]).array())
+app.input.bind("Jump", ["Space", "MouseLeft"])
+app.system((dt, { q, input }) => {
+    if (input.pressed("Jump")) {
+        console.log("Jump!", input.mouse.world)
+    }
+}, 0)
+
+app.start()
--- waffle.js
+++ waffle.js
@@ -33,11 +33,197 @@ function Query(objList) {
         return objList
     }
 }
+export function Input(canvas, { worldFromScreen = null } = {}) {
+    const codeDown = new Set();
+    const actionBindings = new Map();
+    let axes = new Map();
 
-export function World(canvas) {
+    let lastActionsDown = new Set();
+    let actionsDown = new Set();
+    let actionsPressed = new Set();
+    this.mouse = {
+        screen: { x: 0, y: 0 },
+        world: { x: 0, y: 0 },
+        buttons: new Set(), // "MouseLeft" | "MouseMiddle" | "MouseRight"
+    };
+    this.bind = function (action, codes) {
+        actionBindings.set(action, new Set(codes));
+        recomputeActionsDown();
+    };
+    this.bindAdd = function (action, codes) {
+        const s = actionBindings.get(action) ?? new Set();
+        for (const c of codes) s.add(c);
+        actionBindings.set(action, s);
+        recomputeActionsDown();
+    };
+    this.unbind = function (action) {
+        actionBindings.delete(action);
+        actionsDown.delete(action);
+        actionsPressed.delete(action);
+    };
+    this.addAxis = function (name, negativeActions, positiveActions) {
+        axes.set(name, {
+            neg: new Set(negativeActions),
+            pos: new Set(positiveActions),
+        });
+    };
+
+    this.down = function (action) {
+        return actionsDown.has(action);
+    };
+    this.pressed = function (action) {
+        return actionsPressed.has(action);
+    };
+
+
+    this.sample = function () {
+        actionsPressed.clear();
+        for (const a of actionsDown) {
+            if (!lastActionsDown.has(a)) actionsPressed.add(a);
+        }
+        lastActionsDown = new Set(actionsDown);
+    };
+
+    this.axis = function (name) {
+        const ax = axes.get(name);
+        if (!ax) return 0;
+        const neg = anyActionDown(ax.neg);
+        const pos = anyActionDown(ax.pos);
+        if (neg && !pos) return -1;
+        if (pos && !neg) return +1;
+        return 0;
+    };
+
+
+    this.setWorldFromScreen = function (fn) {
+        worldFromScreen = typeof fn === 'function' ? fn : null;
+        updateWorldFromScreen();
+    };
+    this.destroy = function () {
+        detach();
+    };
+
+    function anyActionDown(actionSet) {
+        for (const a of actionSet) if (actionsDown.has(a)) return true;
+        return false;
+    }
+    function recomputeActionsDown() {
+        actionsDown.clear();
+        for (const [action, codes] of actionBindings) {
+            for (const c of codes) {
+                if (codeDown.has(c)) {
+                    actionsDown.add(action);
+                    break;
+                }
+            }
+        }
+    }
+    function updateMouseScreen(e) {
+        const r = canvas.getBoundingClientRect();
+        const x = e.clientX - r.left;
+        const y = e.clientY - r.top;
+        thisObj.mouse.screen.x = x;
+        thisObj.mouse.screen.y = y;
+        updateWorldFromScreen();
+    }
+    function updateWorldFromScreen() {
+        if (worldFromScreen) {
+            const p = worldFromScreen(thisObj.mouse.screen);
+            if (p && typeof p.x === 'number' && typeof p.y === 'number') {
+                thisObj.mouse.world.x = p.x;
+                thisObj.mouse.world.y = p.y;
+            }
+        } else {
+            thisObj.mouse.world.x = thisObj.mouse.screen.x;
+            thisObj.mouse.world.y = thisObj.mouse.screen.y;
+        }
+    }
+    function mouseButtonCode(button) {
+        return button === 0 ? "MouseLeft"
+            : button === 1 ? "MouseMiddle"
+            : button === 2 ? "MouseRight"
+            : `Mouse${button}`;
+    }
+    function clearAllStates() {
+        codeDown.clear();
+        thisObj.mouse.buttons.clear();
+        recomputeActionsDown();
+        actionsPressed.clear();
+        lastActionsDown.clear();
+    }
+
+    const thisObj = this;
+
+    function onKeyDown(e) {
+        if (document.activeElement !== canvas) return;
+        if (e.repeat) return;
+        codeDown.add(e.code);
+        recomputeActionsDown();
+    }
+    function onKeyUp(e) {
+        if (document.activeElement !== canvas) return;
+        codeDown.delete(e.code);
+        recomputeActionsDown();
+    }
+    function onMouseDown(e) {
+        canvas.focus();
+        const code = mouseButtonCode(e.button);
+        thisObj.mouse.buttons.add(code);
+        codeDown.add(code);
+        recomputeActionsDown();
+    }
+    function onMouseUp(e) {
+        const code = mouseButtonCode(e.button);
+        thisObj.mouse.buttons.delete(code);
+        codeDown.delete(code);
+        recomputeActionsDown();
+    }
+    function onMouseMove(e) {
+        updateMouseScreen.call(thisObj, e);
+    }
+    function onContextMenu(e) {
+        e.preventDefault();
+    }
+    function onCanvasBlur() {
+        clearAllStates();
+    }
+    function onWindowBlur() {
+        clearAllStates();
+    }
+
+    function attach() {
+        canvas.addEventListener("keydown", onKeyDown);
+        canvas.addEventListener("keyup", onKeyUp);
+        canvas.addEventListener("blur", onCanvasBlur);
+        canvas.addEventListener("mousedown", onMouseDown);
+        canvas.addEventListener("mouseup", onMouseUp);
+        canvas.addEventListener("mousemove", onMouseMove);
+        canvas.addEventListener("contextmenu", onContextMenu);
+        window.addEventListener("blur", onWindowBlur);
+    }
+    function detach() {
+        canvas.removeEventListener("keydown", onKeyDown);
+        canvas.removeEventListener("keyup", onKeyUp);
+        canvas.removeEventListener("blur", onCanvasBlur);
+        canvas.removeEventListener("mousedown", onMouseDown);
+        canvas.removeEventListener("mouseup", onMouseUp);
+        canvas.removeEventListener("mousemove", onMouseMove);
+        canvas.removeEventListener("contextmenu", onContextMenu);
+        window.removeEventListener("blur", onWindowBlur);
+    }
+
+    attach();
+}
+
+
+export function World(canvas, fps = 60) {
     let id2Object = new Map()
     let id = 0
+    let systems = []
+    const dt = 1000.0 / fps
+
     this.canvas = canvas
+    this.input = new Input(canvas)
 
     this.spawn = function ({
         tags, state
@@ -49,5 +235,21 @@ export function World(canvas) {
 
     this.q = function () {
         return new Query([ ...id2Object.values() ])
+    }
+
+    this.system = function (fn, priority) {
+        systems.push([fn, priority])
+        systems.sort((a, b) => a[1] - b[1])
+    }
+
+    this.flush = function () {
+        for (const i of systems) {
+            i[0](dt, { q: this.q(), input: this.input })
+        }
+        this.input.sample()
+    }
+
+    this.start = function () {
+        setInterval(this.flush.bind(this), dt)
     }
 }
